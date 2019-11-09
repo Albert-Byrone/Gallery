b09959829f68ea0fd11f4347aaefbd8f1eac45df
Albert Byrone <albertbyrone1677@gmail.com>
2019-11-09 18:00:52 +0300

    initial commit

---
+++ album/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+import datetime as dt
+
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+
+    def save_location(self):
+        self.save()
+    def delete_location(self):
+        self.delete()
+
+    @classmethod
+    def get_location(cls):
+        location = Location.objects.all()
+        return location
+
+    @classmethod
+    def update_location(cls,id,value):
+        cls.objects.filter(id=id).update(image=value)
+
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name= models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    name = models.CharField(max_length = 60)
+    date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    author = models.CharField(max_length=50)
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+
+    
--- album/views.py
+++ album/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Image,Location,Category
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Home")
+    images = Image.objects.all()[::5]
+    locations =Location.get_location() 
+    return render(request,'album/index.html',{"images":images,"locations":locations})
