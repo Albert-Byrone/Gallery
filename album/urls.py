b6956ce0017eb7f34704e0038c712d7c11e648b7
Albert Byrone <albertbyrone1677@gmail.com>
2019-11-11 11:24:03 +0300

    add thre location url

--- album/urls.py
+++ album/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     url('^$',views.home,name="name"),
     url(r'^search/', views.search_results, name='search_results'),
+    url(r'^location/(?P<location>\w+)',views.locate_image,name='location'),
 ]
