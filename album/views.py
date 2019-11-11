c5b31464894d946f20d07476a8e86ad172bce6f7
Albert Byrone <albertbyrone1677@gmail.com>
2019-11-11 11:22:17 +0300

    create a search function

--- album/views.py
+++ album/views.py
@@ -11,3 +11,14 @@ def home(request):
 def locate_image(request,location):
     images = Image.filter_by_location(location)
     return render(request,'album/location.html',{"located_images":images})
+
+def search_results(request):
+    if 'search' in request.GET and request.GET["search"]:
+        category = request.GET.get("search")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+        
+        return render(request, 'album/search.html', {"message": message, "images": searched_images})
+    else:
+        message = "You haven't searched for any image category"
+        return render(request, 'album/search.html', {"message": message})
